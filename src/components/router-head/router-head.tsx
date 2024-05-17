d61522c8b5fa6425524eaba76003b21c30b6449a
Marco Bellezza <jasone.mb@outlook.com>
2024-05-17 21:28:39 +0200

    change profile pic

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -16,7 +16,7 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="#fff" />
-      <link rel="icon" type="image/jpg" href="/profile-pic.jpg" />
+      <link rel="icon" type="image/jpg" href="/manifest-icon-512.maskable.png" />
       <link rel="apple-touch-icon" href="/ios/192.png"></link>
 
       {head.meta.map((m) => (
